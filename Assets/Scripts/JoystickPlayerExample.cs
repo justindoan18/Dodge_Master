d84447fcae026bc2de8791cd306c3238e4df5a2a
JustinDoan <justindoan1809@gmail.com>
2023-03-07 00:25:01 +0700

    Joystick_applied v1.0

---
+++ Assets/Scripts/JoystickPlayerExample.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float moveSpeed;
+    public FloatingJoystick joystick;
+    public Rigidbody2D rb;
+    public Vector2 move;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        move.x = joystick.Horizontal;
+        move.y = joystick.Vertical;
+    }
+    private void FixedUpdate()
+    {
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float screenWidth = Camera.main.orthographicSize * 2 * screenRatio;
+        float screenHeight = Camera.main.orthographicSize * 2;
+
+        // Giới hạn vị trí của gameObject
+        float newX = Mathf.Clamp(rb.position.x + move.x * moveSpeed * Time.deltaTime, -screenWidth / 2, screenWidth / 2);
+        float newY = Mathf.Clamp(rb.position.y + move.y * moveSpeed * Time.deltaTime, -screenHeight / 2, screenHeight / 2);
+        rb.MovePosition(new Vector2(newX,newY));        
+    }
+}
